1ac61d0af9b5a879da344fd0b0303aa1768ee502
Nazariy Vekluch <nazariyvekluch@gmail.com>
2017-04-19 00:23:05 +0300

    Add UI dynamic grid, X and O sprites

---
+++ Assets/Scripts/UI/DynamicGrid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DynamicGrid : MonoBehaviour
+{
+    public int Row, Column;
+
+    private IEnumerator _corontine;
+
+    IEnumerator ChangeCellSize()
+    {
+        yield return new WaitForEndOfFrame();
+
+        RectTransform parent = gameObject.GetComponent<RectTransform>();
+        GridLayoutGroup grid = gameObject.GetComponent<GridLayoutGroup>();
+
+        grid.cellSize = new Vector2((parent.rect.width - grid.padding.left - grid.padding.right - grid.spacing.x * (Column - 1)) / Column,
+                                    (parent.rect.height - grid.padding.top - grid.padding.bottom - grid.spacing.y * (Row - 1)) / Row);
+
+        _corontine = null;
+    }
+
+    private void OnRectTransformDimensionsChange()
+    {
+        if (_corontine == null)
+        {
+            _corontine = ChangeCellSize();
+
+            if (gameObject.activeInHierarchy)
+                StartCoroutine(_corontine);
+        }
+    }
+}
